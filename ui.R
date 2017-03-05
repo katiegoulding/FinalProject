e6d3db48bbf93636fbf258de0993c64e44fe109c
Parker Singh <rekrapsingh@gmail.com>
2017-03-05 13:54:24 -0800

    Add widgets to second vis

--- ui.R
+++ ui.R
@@ -9,8 +9,24 @@ simpleSpeedDating.df <- read.csv("data/simpleSpeedDating.df.csv", stringsAsFacto
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      selectInput("")
-    )
+      selectInput("racial.group.1", label = h5("Select a racial group:"), 
+                  choices = list("Black/African American",
+                                 "European/Caucasian American",
+                                 "Latino/Hispanic American",
+                                 "Asian/Pacific Islander/Asian-American",
+                                 "Other"), selected = "Black/African American"),
+      selectInput("racial.group.2", label = h5("Select another racial group to compare:"), 
+                  choices = list("Black/African American",
+                                 "European/Caucasian American",
+                                 "Latino/Hispanic American",
+                                 "Asian/Pacific Islander/Asian-American",
+                                 "Other"), selected = "European/Caucasian American"),
+      radioButtons("interest.select", label = h5("Select an Interest:"),
+                   choices = list("Attribute Rating", "Hobbies", "Importance of Race or Religion"),
+                   selected = NULL, inline = FALSE
+      )
+    ),
+    mainPanel()
   )
 )
 

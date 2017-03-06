c2f27d08d758f34abd49e725d509cdc7a2537dae
Parker Singh <rekrapsingh@gmail.com>
2017-03-05 16:04:09 -0800

    Add comments and format code

--- server.R
+++ server.R
@@ -5,29 +5,46 @@ library(plotly)
 library(tidyr)
 
 # setwd("~/Desktop/INFO 201 Autumn 17 HW/FinalProject")
-simpleSpeedDating.df <- read.csv("data/simpleSpeedDating.df.csv", stringsAsFactors = FALSE)
+simpleSpeedDating.df <-
+  read.csv("data/simpleSpeedDating.df.csv", stringsAsFactors = FALSE)
 
 server <- function(input, output) {
+  #Create reactive data for plot based on widgets
+  first.vis.data <- reactive({
+    first.vis.speedDating <- simpleSpeedDating.df
+    
+    #Filter data based on sex
+    if (input$sex_select != "All") {
+      first.vis.speedDating <-
+        filter(first.vis.speedDating, Sex == input$sex_select)
+    }
+    
+    #Gathers the percentage of matches for selected racial group
+    first.vis.speedDating <-
+      filter(first.vis.speedDating, Race == input$racial_group) %>%
+      group_by(Race.of.Partner, Match) %>%
+      summarize("Interactions" = n()) %>%
+      mutate("Total Interactions" = sum(Interactions)) %>%
+      filter(Match == "Yes" &
+               Race.of.Partner != "NA") %>%
+      mutate("Percentage" = (Interactions / `Total Interactions`) * 100)
+    return(first.vis.speedDating)
+  })
   
- first.vis.data <- reactive({
-   first.vis.speedDating <- simpleSpeedDating.df
-   if(input$sex_select != "All") {
-     first.vis.speedDating <- filter(first.vis.speedDating, Sex == input$sex_select)
-   }
-   
-   first.vis.speedDating <- filter(first.vis.speedDating, Race == input$racial_group) %>%
-                            group_by(Race.of.Partner, Match) %>%
-                            summarize("Interactions" = n()) %>%
-                            mutate("Total Interactions" = sum(Interactions)) %>%
-                            filter(Match == "Yes" & Race.of.Partner != "NA") %>%
-                            mutate("Percentage" = (Interactions / `Total Interactions`) * 100)
-   return(first.vis.speedDating)
- })
- 
-output$first.vis <- renderPlotly({
+  #Render data in plotly bar  chart
+  output$first.vis <- renderPlotly({
+    plot_ly(
+      first.vis.data(),
+      x = ~ Race.of.Partner,
+      y = ~ Percentage,
+      type = "bar"
+    ) %>%
+      layout(
+        xaxis = list(title = "Race of Partner", tickangle = 15),
+        yaxis = list(title = "Match Percentage"),
+        margin = 100
+      )
+  })
   
-  plot_ly(first.vis.data(), x = ~Race.of.Partner, y = ~Percentage, type = "bar") %>%
-    layout(xaxis = list(title = "Race of Partner",tickangle = 15), yaxis = list(title = "Match Percentage"), margin = 100)
-})
 }
 shinyServer(server)
--- ui.R
+++ ui.R
@@ -3,7 +3,8 @@ library(dplyr)
 library(plotly)
 library(ggplot2)
 
-simpleSpeedDating.df <- read.csv("data/simpleSpeedDating.df.csv", stringsAsFactors = FALSE)
+simpleSpeedDating.df <-
+  read.csv("data/simpleSpeedDating.df.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
   titlePanel(title = "Speed Dating Data"),
@@ -11,24 +12,31 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       # Select Box for graph
-        #'input.dataset === ""',
-        selectInput("racial_group", label = h5("Select a racial group"), 
-                    choices = list("Black/African American",
-                                   "European/Caucasian American",
-                                   "Latino/Hispanic American",
-                                   "Asian/Pacific Islander/Asian-American",
-                                   "Other"), selected = "Black/African American"),
-        radioButtons("sex_select", label = h5("Select a Sex:"),
-                     choices = list("All", "Female", "Male"),
-                     selected = NULL, inline = FALSE
-        )
+      #'input.dataset === ""',
+      #Dropdown menu to select racial group
+      selectInput(
+        "racial_group",
+        label = h5("Select a racial group"),
+        choices = list(
+          "Black/African American",
+          "European/Caucasian American",
+          "Latino/Hispanic American",
+          "Asian/Pacific Islander/Asian-American",
+          "Other"
+        ),
+        selected = "Black/African American"
       ),
+      #Radio buttons to select gender
+      radioButtons(
+        "sex_select",
+        label = h5("Select a Sex:"),
+        choices = list("All", "Female", "Male"),
+        selected = NULL,
+        inline = FALSE
+      )
+    ),
     
-    mainPanel(
-      p("This is the first visualization"),
-      tableOutput("table"),
-      plotlyOutput("first.vis")
-    )
+    mainPanel(plotlyOutput("first.vis"))
   )
 )
 

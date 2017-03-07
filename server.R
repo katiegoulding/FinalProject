688aa9ab616e7285b8fc92038e548bec340c9666
Parker Singh <rekrapsingh@gmail.com>
2017-03-06 16:31:53 -0800

    Change column names and change right margin of plot

--- server.R
+++ server.R
@@ -8,6 +8,16 @@ simpleSpeedDating.df <- read.csv("data/simpleSpeedDating.df.csv", stringsAsFacto
 
 #View(simpleSpeedDating.df)
 
+#Change column names to make plot more readable to user
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Preference.of.Partner.Attract")] <- "Partner Attractiveness"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Preference.of.Partner.Sincerity")] <- "Partner Sincerity"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Preference.of.Partner.Intelligence")] <- "Partner Intelligence"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Preference.of.Partner.Fun")] <- "Partner Fun"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Preference.of.Partner.Ambition")] <- "Partner Ambition"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Preference.of.Partner.Shared.Interest")] <- "Partner Shared Interest"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Importance.Same.Race")] <- "Importance of Same Race"
+colnames(simpleSpeedDating.df)[which(names(simpleSpeedDating.df) == "Importance.Same.Religion")] <- "Importance of Same Religion"
+
 server <- function(input, output, session) {
   second.vis.data <- reactive({
     speed.dating.df <- simpleSpeedDating.df
@@ -16,13 +26,13 @@ server <- function(input, output, session) {
     
     #Selecting interest
     if(input$interest.select == "Attribute Rating") {
-      speed.dating.df <- select(speed.dating.df, Race, Preference.of.Partner.Attract:Preference.of.Partner.Shared.Interest)
+      speed.dating.df <- select(speed.dating.df, Race, `Partner Attractiveness`:`Partner Shared Interest`)
     }
     if(input$interest.select == "Hobbies") {
       speed.dating.df <- select(speed.dating.df, Race, Sports:Yoga)
     }
     if(input$interest.select == "Importance of Race or Religion") {
-      speed.dating.df <- select(speed.dating.df, Race, Importance.Same.Race, Importance.Same.Religion)
+      speed.dating.df <- select(speed.dating.df, Race, `Importance of Same Race`, `Importance of Same Religion`)
     }
     
     #Convert df to long format
@@ -55,7 +65,7 @@ server <- function(input, output, session) {
   
  output$second.vis <- renderPlotly({
    plot_ly(second.vis.data(), x = ~interest, y = ~Median, type = "bar", color = ~Race) %>%
-   layout(margin = list(b = 150), yaxis = list(range = c(0, yaxis.max())))
+   layout(margin = list(b = 150, r = 100), yaxis = list(range = c(0, yaxis.max())))
  })
 
 

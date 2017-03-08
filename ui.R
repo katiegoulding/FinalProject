8660d87a3861a5ca92629786c6692faf28830259
Katie Goulding <katiegoulding@gmail.com>
2017-03-07 16:12:27 -0800

    Add color by gender and changes to plot)

--- ui.R
+++ ui.R
@@ -9,24 +9,18 @@ simpleSpeedDating.df <- read.csv("data/simpleSpeedDating.df.csv", stringsAsFacto
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
-      sliderInput("confidence", label = h5("Select confidence range:"), 
-                  min = 0,
-                  max = 10,
-                  value = 0
+      sliderInput("confidence", label = h5("Select confidence range:"), min = 0,  max = 1, value = c(0, 1)
       ),
-                  
-      sliderInput("success", label = h5("Select sucess range:"), 
-                  min = 0,
-                  max = 10,
-                  value = 0
-                  
+      
+      sliderInput("success", label = h5("Select success range:"), min = 0,  max = 1, value = c(0, 1)
       )
     ),
     mainPanel(
       plotlyOutput("third.vis")
     )
   )
- )
+)
+
 
 
 shinyUI(ui)

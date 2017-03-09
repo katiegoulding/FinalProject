532336becac70eebcba10d1f9531719523ab6621
Parker Singh <rekrapsingh@gmail.com>
2017-03-08 19:40:43 -0800

    Adjust and create a new tabs

--- server.R
+++ server.R
@@ -4,17 +4,12 @@ library(ggplot2)
 library(plotly)
 library(tidyr)
 
-# setwd("~/Desktop/INFO 201 Autumn 17 HW/FinalProject")
+
 speed.data <- read.csv("data/simpleSpeedDating.df.csv")
-View(speed.data)
+
 
 server <- function(input, output) {
   
-  function(input, output) {
-
-    output$table <- renderDataTable({
-      speed.data
-    })
-  }
+  
 }
 shinyServer(server)

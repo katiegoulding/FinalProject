0e717f82bd64a7568fd83f6892f08e93377f8877
Parker Singh <rekrapsingh@gmail.com>
2017-03-08 19:18:39 -0800

    Coord Flip visualization

--- server.R
+++ server.R
@@ -23,65 +23,33 @@ server <- function(input, output) {
         # Creates df with percent breakdown
         race.df <- mutate(count.per.race, percent.race = round(count/total.participants * 100, 2))
         # Creates an x-axis title
-        x <- list(
-          title = "Race/Gender"
+        y <- list(
+          title = ""
         )
         # Creates a y-axis title
-        y <- list(
+        x <- list(
           title = "Percent"
         )
         # Creates a title for the race bar graph ***does not work***
         #title.race <- list(
         #  title = "Race Percent Breakdown of All Participants"
         #)
+        
+        pal <- c("#006bfa", "#ede800")
+        
         # Produces plotly bar graph of racial breakdown
         race.graph <- plot_ly(
-          x = race.df$Race,
-          y = race.df$percent.race,
+          x = race.df$percent.race,
+          y = race.df$Race,
           name = "Participant Racial Breakdown",
           type = "bar",
+          orientation = "h",
           color = race.df$Sex,
+          colors = pal,
           showlegend = TRUE) %>% 
           #layout(title = title.race, xaxis = x, yaxis = y, margin = list(b = 150, r = 30))
-          layout(xaxis = x, yaxis = y, margin = list(b = 150, r = 30))
+          layout(xaxis = x, yaxis = y, margin = list(b = 150, r = 30, l = 200))
     })
-
-  
-
-    
-    # output$sex.graph <- renderPlotly({
-    #   count.per.sex <- select(simpleSpeedDating.df, ID, Sex) %>% 
-    #     unique() %>% 
-    #     group_by(Sex) %>% 
-    #     summarize(count = n()) %>% 
-    #     drop_na()
-    #   total.participants <- sum(count.per.sex$count)
-    #   # Creates df with percent breakdown
-    #   sex.df <- mutate(count.per.sex, ratio.sex = (count))
-    #   # Creates an x-axis title
-    #   x <- list(
-    #     title = "Sex"
-    #   )
-    #   # Creates a y-axis title
-    #   y <- list(
-    #     title = "Number of persons identified with respective gender"
-    #   )
-    #   # Creates a title for the sex bar graph ***does not work***
-    #   title.sex <- list(
-    #     title = "Sex breakdown of all participants"
-    #   )
-    #   # Produces plotly bar graph of sex breakdown
-    #   sex.graph <- plot_ly(
-    #     x = sex.df$Sex,
-    #     y = sex.df$ratio.sex,
-    #     name = "Participant Sex Breakdown",
-    #     type = "bar",
-    #     color = sex.df$Sex,
-    #     showlegend = FALSE) %>% 
-    #     layout(title = title.sex, xaxis = x, yaxis = y)
-    #   })
-      
-    
     
 }
 shinyServer(server)
